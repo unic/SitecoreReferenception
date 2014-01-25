13a343badb21c1263012566783e185985d86b828
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-01-25 07:06:29 +0100

    Implement UsageReferenceProvider and make test output recursive.

--- src/Referenception.Common/UsageReferencesProvider.cs
+++ src/Referenception.Common/UsageReferencesProvider.cs
@@ -7,13 +7,24 @@ using System.Threading.Tasks;
 namespace Referenception.Common
 {
     using Referenception.Core;
+    using Referenception.Core.Extensions;
     using Referenception.Core.Nodes;
 
     public class UsageReferencesProvider : ReferenceProviderBase
     {
         public override IEnumerable<INode> GetChildren()
         {
-            throw new NotImplementedException();
+            var items = this.Context.Item.GetLinkedItems();
+            foreach (var item in items)
+            {
+                var node = new ItemNode
+                           {
+                               Context = new ReferenceContext { Item = item },
+                               DisplayName = item.DisplayName
+                           };
+
+                yield return node;
+            }
         }
     }
 }
