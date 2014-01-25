deef365f9614d32203878b5ac9d94051e7957eed
Tobias Studer <tobias.studer@unic.com>
2014-01-25 05:03:51 +0100

    Added empty test project.

--- src/Referenception.Common/CloneReferencesProvider.cs
+++ src/Referenception.Common/CloneReferencesProvider.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Referenception.Common
+﻿namespace Referenception.Common
 {
+    using System;
+    using System.Collections.Generic;
     using Referenception.Core;
 
     public class CloneReferencesProvider : ReferenceProviderBase
