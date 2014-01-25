80f4cf4b9484549cc902994867d3ca87e82b7a65
Tobias Studer <tobias.studer@unic.com>
2014-01-25 13:47:06 +0100

    Added tooltip.

--- src/Referenception.Core/Data/DataRow.cs
+++ src/Referenception.Core/Data/DataRow.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Referenception.Core.Data
+﻿namespace Referenception.Core.Data
 {
+    using System.Globalization;
+    using System.Linq;
     using Sitecore.Data.Items;
 
     public class DataRow
@@ -17,5 +13,21 @@ namespace Referenception.Core.Data
         public string Id { get; set; }
 
         public string TemplateName { get; set; }
+
+        public virtual Tooltip GetTooltip()
+        {
+            return new Tooltip
+                       {
+                           DisplayName = this.DisplayName,
+                           Id = this.Id,
+                           TemplateName = this.TemplateName,
+                           TemplateId = this.Item.TemplateID.ToString(),
+                           Created = this.Item.Statistics.Created.ToLongTimeString(),
+                           Updated = this.Item.Statistics.Updated.ToLongTimeString(),
+                           Revision = this.Item.Statistics.Revision,
+                           Languages = this.Item.Languages.Select(l => l.ToString()),
+                           Versions = this.Item.Versions.Count.ToString(CultureInfo.InvariantCulture)
+                       };
+        }
     }
 }
